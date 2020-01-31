1ecb465c84308c23cf8c58798e82c8375aa15060
Jose Thome <josethome.band@hotmail.com>
2020-01-31 17:02:43 -0200

    feature/adicionado-módulo-postagens-e-módulo-usuarios

--- test.js
+++ test.js
@@ -4,3 +4,41 @@ const sequelize = new Sequelize('test', 'root', '', {
 	dialect: 'mysql'
 })
 
+const Postagem = sequelize.define('postagens', {
+	titulo: {
+		type: Sequelize.STRING
+	},
+	conteudo: {
+		type: Sequelize.TEXT
+	}
+})
+
+// Postagem.create({
+// 	titulo: "UM TITULO PARA POSTAGEM",
+// 	conteudo: "WQEURPOEROERKREKERKREKRKERKEMEMMEMEMENEWENEWNENENFRNERNENEWNDSNDSNDSNN"
+// })
+
+const Usuario = sequelize.define('usuarios', {
+	nome: {
+		type: Sequelize.STRING
+	},
+	sobrenome: {
+		type: Sequelize.STRING
+	},
+	data_nascimento: {
+		type: Sequelize.DATE
+	},
+	email: {
+		type: Sequelize.STRING
+	}
+})
+
+Usuario.create({
+	nome: "José",
+	sobrenome: "Thomé",
+	data_nascimento: "1998-01-21",
+	email: "josethome.band@bol.com"
+})
+
+//Usuario.sync({force: true})
+// Postagem.sync({force: true})
